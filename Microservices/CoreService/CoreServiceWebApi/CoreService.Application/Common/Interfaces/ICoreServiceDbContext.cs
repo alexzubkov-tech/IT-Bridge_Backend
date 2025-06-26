72463aedf307037ebd5a0ee8719539e6bfffc630
Alex Zubkov <alex.zzubkoff@gmail.com>
2025-06-26 17:06:28 +0500

    Создал слой Application, интерфейс DbContext, начал реализовывать CQRS

---
+++ Microservices/CoreService/CoreServiceWebApi/CoreService.Application/Common/Interfaces/ICoreServiceDbContext.cs
@@ -0,0 +1,23 @@
+﻿using CoreService.Domain.Entities;
+using Microsoft.EntityFrameworkCore;
+
+namespace CoreService.Application.Common.Interfaces
+{
+    public interface ICoreServiceDbContext
+    {
+        DbSet<Answer> Answers { get; set; }
+        DbSet<AnswerRating> AnswerRatings { get; set; }
+        DbSet<Category> Categories { get; set; }
+        DbSet<CategoryQuestion> CategoryQuestions { get; set; }
+        DbSet<CommentAnswer> CommentAnswers { get; set; }
+        DbSet<CommentQuestion> CommentQuestions { get; set; }
+        DbSet<Company> Companies { get; set; }
+        DbSet<Question> Questions { get; set; }
+        DbSet<QuestionRating> QuestionRatings { get; set; }
+        DbSet<QuestionTag> QuestionTags { get; set; }
+        DbSet<Tag> Tags { get; set; }
+        DbSet<User> Users { get; set; }
+        DbSet<UserProfile> UserProfiles { get; set; }
+        Task<int> SaveChangesAsync(CancellationToken cancellationToken = default);
+    }
+}
